2f8064405194c268764512cfd9ac4c4767c1c576
Felipe Duarte <felipesdrs@hotmail.com>
2026-05-13 10:20:27 -0300

    xdg: directories are set by default

--- home/common/core/xdg.nix
+++ home/common/core/xdg.nix
@@ -1,18 +1,6 @@
-{ config, ... }:
-let
-  Home = config.home.homeDirectory;
-in
 {
   xdg.userDirs = {
     enable = true;
     createDirectories = true;
-
-    desktop = "${Home}/Desktop";
-    download = "${Home}/Downloads";
-    music = "${Home}/Music";
-    pictures = "${Home}/Pictures";
-    publicShare = "${Home}/Public";
-    templates = "${Home}/Templates";
-    videos = "${Home}/Videos";
   };
 }
