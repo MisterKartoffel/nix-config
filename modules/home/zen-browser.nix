35c989bc22579fd4a3996d232261b415d59be3c6
Felipe Duarte <felipesdrs@hotmail.com>
2026-05-21 15:35:26 -0300

    zen-browser: disabled translations

--- modules/home/zen-browser.nix
+++ modules/home/zen-browser.nix
@@ -150,6 +150,7 @@ in
         "theme.better_find_bar.enable_custom_background" = true;
         "theme.better_find_bar.custom_background" = "#1e1e2e";
 
+        "browser.translations.enable" = false;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "zen.view.compact.enable-at-startup" = true;
         "zen.view.experimental-no-window-controls" = true;
