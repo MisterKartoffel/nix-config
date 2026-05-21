89c891b04e9e1eb2f978c55532f5410e49a4ed07
Felipe Duarte <felipesdrs@hotmail.com>
2026-05-21 15:35:05 -0300

    zen-browser: installed stylus extension

--- modules/home/zen-browser.nix
+++ modules/home/zen-browser.nix
@@ -57,6 +57,7 @@ in
           "{446900e4-71c2-419f-a6a7-df9c091e268b}" = "bitwarden-password-manager";
           "uBlock0@raymondhill.net" = "ublock-origin";
           "@testpilot-containers" = "multi-account-containers";
+          "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = "styl-us";
         }
         // {
           "*".installation_mode = "blocked";
