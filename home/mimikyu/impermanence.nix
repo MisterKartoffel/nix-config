8adf3670ad9852488cadc5bcab28e72acad289b9
Felipe Duarte <felipesdrs@hotmail.com>
2026-05-21 15:34:42 -0300

    impermanence: optionalized home-manager

--- home/mimikyu/impermanence.nix
+++ home/mimikyu/impermanence.nix
@@ -1,30 +1,63 @@
+{ config, lib, ... }:
+let
+  inherit (config.xdg) userDirs;
+  inherit (config.accounts.email) maildirBasePath;
+  inherit (config.services) gnome-keyring;
+  inherit (config.programs)
+    offlineimap
+    ssh
+    vesktop
+    zen-browser
+    zsh
+    ;
+in
 {
   home.persistence."/etc/persist" = {
     hideMounts = true;
 
-    directories = [
-      "Desktop"
-      "Documents"
-      "Downloads"
-      "Mail"
-      "Music"
-      "Pictures"
-      "Projects"
-      "Public"
-      "Templates"
-      "Videos"
-      ".config/zen"
-      ".config/vesktop"
-      {
-        directory = ".local/share/keyrings";
-        mode = "0700";
-      }
-    ];
+    directories =
+      lib.optionals (userDirs.enable && userDirs.createDirectories) (
+        map baseNameOf (
+          builtins.filter (path: path != null) (
+            builtins.attrValues {
+              inherit (userDirs)
+                desktop
+                documents
+                download
+                music
+                pictures
+                projects
+                publicShare
+                templates
+                videos
+                ;
+            }
+          )
+        )
+      )
+      ++ lib.optionals offlineimap.enable [
+        (baseNameOf maildirBasePath)
+      ]
+      ++ lib.optionals vesktop.enable [
+        ".config/vesktop"
+      ]
+      ++ lib.optionals zen-browser.enable [
+        ".config/zen"
+      ]
+      ++ lib.optionals gnome-keyring.enable [
+        {
+          directory = ".local/share/keyrings";
+          mode = "0700";
+        }
+      ];
 
-    files = [
-      ".ssh/known_hosts"
-      ".config/zsh/.p10k.zsh"
-      ".local/state/zsh/history"
-    ];
+    files =
+      lib.optionals ssh.enable [
+        ".ssh/known_hosts"
+      ]
+      ++ lib.optionals zsh.enable [
+        ".config/zsh/.p10k.zsh"
+        ".local/state/zsh/history"
+      ];
   };
 }
