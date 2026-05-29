edb099b53a13ed6a9c5d85ab10a53ce38a344a7c
Felipe Duarte <felipesdrs@hotmail.com>
2026-05-29 15:34:32 -0300

    users: removed lib.mkMerge semantics

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -13,24 +13,24 @@ in
   services.userborn.enable = true;
 
   users.mutableUsers = false;
-  users.users = lib.mkMerge [
+  users.users =
     (lib.mapAttrs (username: user: {
       isNormalUser = true;
-
       inherit (user) shell extraGroups;
       description = lib.mkIf sops.enable secrets.home.${username}.name;
+
+      hashedPasswordFile = lib.mkIf sops.enable secrets.host.${username}.password.path;
       initialHashedPassword = lib.mkIf (
         !sops.enable
       ) "$y$j9T$9lxUmIACkk7jFAU437ubP/$/dbwqUcskqwzxBC.Lg7WJx4uf/8jxLGcxRjM36U0q57";
-      hashedPasswordFile = lib.mkIf sops.enable secrets.host.${username}.password.path;
 
       openssh.authorizedKeys.keys = [
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBJRyJ3RdkVQsdZpnQ0+hPPwzI+lg9XprrK3ntSFPldhBsA4sywtAy4U2P+9DtdeON29opxsUyiDd2yprr2iwWG8= termius@s20fe"
       ];
     }) system.users)
-
-    { root.initialPassword = "!"; }
-  ];
+    // {
+      root.initialPassword = "!";
+    };
 
   home-manager.users = lib.mapAttrs (username: _: {
     imports = builtins.concatMap importTree [
