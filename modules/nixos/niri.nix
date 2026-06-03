e71dbf0d06a0826d3ec849910e5364d4fe38b042
Felipe Duarte <felipesdrs@hotmail.com>
2026-06-02 22:30:18 -0300

    niri: fixed redundant check

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -16,7 +16,7 @@ in
       extraPortals = builtins.attrValues { inherit (pkgs) xdg-desktop-portal-gtk; };
     };
 
-    environment.loginShellInit = lib.optionalString niri.enable ''
+    environment.loginShellInit = ''
       if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
       	exec niri-session -l
       fi
