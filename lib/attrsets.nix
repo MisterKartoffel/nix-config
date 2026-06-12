5da442a0d555c1fdfb1412288360c88f61013c7a
Felipe Duarte <felipesdrs@hotmail.com>
2026-06-12 10:22:20 -0300

    lib.custom: added error handling to nestAttrset

--- lib/attrsets.nix
+++ lib/attrsets.nix
@@ -1,13 +1,26 @@
 { lib }:
 let
-  inherit (lib) foldlAttrs recursiveUpdate splitString;
-  inherit (lib.attrsets) setAttrByPath;
+  inherit (lib)
+    concatStringsSep
+    mapAttrsToList
+    recursiveUpdateUntil
+    setAttrByPath
+    splitString
+    ;
 in
 {
   nestAttrset =
     attrset:
-    foldlAttrs (
-      acc: path: value:
-      recursiveUpdate acc (setAttrByPath (splitString "/" path) value)
-    ) { } attrset;
+    builtins.foldl' (recursiveUpdateUntil (
+      path: left: right:
+      if !(builtins.isAttrs left && builtins.isAttrs right) then
+        throw ''
+          Conflict at path '${concatStringsSep "." path}'.
+          Cannot merge duplicate leaves.
+          	- ${builtins.toJSON left}
+          	- ${builtins.toJSON right}
+        ''
+      else
+        false
+    )) { } (mapAttrsToList (key: value: setAttrByPath (splitString "/" key) value) attrset);
 }
