5b3e80b06e11ae27fcc28b714b7203a5fdab20d3
Felipe Duarte <felipesdrs@hotmail.com>
2026-06-15 16:50:17 -0300

    networking: simplified networking configuration

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -1,76 +1,34 @@
-{ config, lib, ... }:
+{ config, ... }:
 let
-  inherit (config) networking;
-  ifaces = config.hardware.facter.detected.dhcp.interfaces;
-  bondName = "bond0";
+  inherit (config.hardware.facter.detected.dhcp) interfaces;
 in
 {
-  systemd.network = lib.mkIf networking.useNetworkd (
-    lib.mkMerge [
-      {
-        wait-online.anyInterface = true;
-      }
-      (lib.mkIf (ifaces != [ ]) {
-        netdevs = {
-          "10-${bondName}" = {
-            netdevConfig = {
-              Kind = "bond";
-              Name = bondName;
-            };
-            bondConfig = {
-              Mode = "active-backup";
-              PrimaryReselectPolicy = "better";
-              MIIMonitorSec = 100;
-            };
-          };
-        };
+  networking = {
+    nameservers = [
+      "1.1.1.2#security.cloudflare-dns.com"
+      "9.9.9.9#tls://dns.quad9.net"
+    ];
 
-        networks =
-          lib.listToAttrs (
-            map (iface: {
-              name = "40-${iface}";
-              value = {
-                matchConfig.Name = iface;
-                networkConfig.Bond = bondName;
-              };
-            }) ifaces
-          )
-          // {
-            "50-${bondName}" = {
-              matchConfig.Name = bondName;
-              linkConfig = {
-                RequiredForOnline = "carrier";
-                MACAddress = "06:F7:E2:F0:75:74"; # locally administered, random
-              };
-              networkConfig = {
-                BindCarrier = lib.concatStringsSep " " ifaces;
-                DHCP = "yes";
-              };
-            };
-          };
-      })
-    ]
-  );
+    bonds.bond0 = {
+      inherit interfaces;
+
+      driverOptions = {
+        mode = "active-backup";
+        primary_reselect = "better";
+        miimon = "100";
+      };
+    };
+
+    interfaces.bond0.macAddress = "06:F7:E2:F0:75:74";
+  };
 
   services.resolved = {
     enable = true;
 
     settings.Resolve = {
-      DNS = [
-        "1.1.1.2#security.cloudflare-dns.com"
-        "9.9.9.9#tls://dns.quad9.net"
-      ];
-
-      FallbackDNS = [
-        "8.8.8.8#dns.google"
-      ];
-
+      FallbackDNS = [ "8.8.8.8#dns.google" ];
       DNSOverTLS = true;
       DNSSEC = true;
     };
-  };
-
-  networking = lib.mkIf (networking.useNetworkd -> ifaces == [ ]) {
-    networkmanager.enable = true;
   };
 }
