f90a97b1c53453a63c6519c382d1025320031c3b
Felipe Duarte <felipesdrs@hotmail.com>
2026-06-16 15:06:55 -0300

    ssh: permanently added github.com public key

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -9,6 +9,9 @@ in
       enable = true;
       generateHostKeys = !impermanence.enable;
 
+      knownHosts."github.com".publicKey =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+
       settings = {
         PermitRootLogin = "no";
         PasswordAuthentication = false;
