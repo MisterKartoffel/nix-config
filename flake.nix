b3e4fa60cc2cbdf666fd5d78f4b862c46a11cf04
Felipe Duarte <felipesdrs@hotmail.com>
2026-06-24 14:35:57 -0300

    flake.nix: simplified

--- flake.nix
+++ flake.nix
@@ -1,38 +1,29 @@
 {
-  description = "Kartoffel tries NixOS 2 - Declarative Boogaloo";
-
   outputs =
     _:
     let
       inputs = import ./.tack;
       lib = inputs.nixpkgs.lib.extend (_: prev: { custom = import ./lib { lib = prev; }; });
       forAllSystems =
-        f: lib.genAttrs lib.systems.flakeExposed (system: f system inputs.nixpkgs.legacyPackages.${system});
-
-      modules =
-        hostname:
-        lib.custom.importTree "modules/nixos"
-        ++ [
-          inputs.home-manager.nixosModules.default
-          inputs.impermanence.nixosModules.default
-          inputs.disko.nixosModules.default
-          ./hosts/${hostname}
-        ];
+        f: lib.genAttrs lib.systems.flakeExposed (system: f inputs.nixpkgs.legacyPackages.${system});
     in
     {
       nixosConfigurations = builtins.mapAttrs (
         hostname: _:
         lib.nixosSystem {
           inherit lib;
-          modules = modules hostname;
+          modules = lib.custom.importTree "modules/nixos" ++ [
+            inputs.home-manager.nixosModules.default
+            inputs.impermanence.nixosModules.default
+            inputs.disko.nixosModules.default
+            ./hosts/${hostname}
+          ];
           specialArgs = { inherit inputs; };
         }
       ) (builtins.readDir ./hosts);
 
-      devShells = forAllSystems (
-        system: pkgs: {
-          default = pkgs.callPackage ./shell.nix { inherit inputs system; };
-        }
-      );
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./shell.nix { inherit inputs; };
+      });
     };
 }
