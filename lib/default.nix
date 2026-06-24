94517d885c55ffb91c8c7b22a6431a41a5ed78d6
Felipe Duarte <felipesdrs@hotmail.com>
2026-06-24 14:35:44 -0300

    lib.custom: improved clarity

--- lib/default.nix
+++ lib/default.nix
@@ -1,17 +1,18 @@
 { lib }:
-let
-  files = builtins.attrNames (removeAttrs (builtins.readDir ./.) [ "default.nix" ]);
-in
-builtins.foldl' (
-  acc: file:
-  let
-    set = import ./${file} { inherit lib; };
-  in
-  builtins.foldl' (
-    acc': name:
-    if builtins.hasAttr name acc' then
-      throw "Duplicate function: '${name}'"
-    else
-      acc' // { ${name} = set.${name}; }
-  ) acc (builtins.attrNames set)
-) { } files
+builtins.foldl'
+  (
+    acc: file:
+    builtins.foldl' (
+      acc': function:
+      if builtins.hasAttr function acc' then
+        throw "lib: duplicate function '${function}'"
+      else
+        acc' // { ${function} = file.${function}; }
+    ) acc (builtins.attrNames file)
+  )
+  { }
+  (
+    map (file: import ./${file} { inherit lib; }) (
+      builtins.attrNames (removeAttrs (builtins.readDir ./.) [ "default.nix" ])
+    )
+  )
