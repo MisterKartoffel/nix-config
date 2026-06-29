df6c9db6f4e2905188892f69480e8805ccd916fa
Felipe Duarte <felipesdrs@hotmail.com>
2026-06-29 10:53:03 -0300

    direnv: fixed incorrect nesting

--- modules/home/direnv.nix
+++ modules/home/direnv.nix
@@ -2,10 +2,12 @@
   programs.direnv = {
     nix-direnv.enable = true;
 
-    config.global = {
-      strict_env = true;
-      warn_timeout = "1m";
-      hide_env_diff = true;
+    config = {
+      global = {
+        strict_env = true;
+        warn_timeout = "1m";
+        hide_env_diff = true;
+      };
 
       whitelist.exact = map (project: "/home/mimikyu/Projects/${project}") [
         "nix-config"
