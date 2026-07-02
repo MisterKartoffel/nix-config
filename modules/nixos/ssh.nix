90ba55051d18ae280165642ba7df51b7bc01d6ae
Felipe Duarte <felipesdrs@hotmail.com>
2026-07-02 14:51:52 -0300

    nixos/ssh: fixed generateHostKeys option

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -2,10 +2,11 @@
 let
   inherit (config.modules.services) impermanence;
   inherit (config.services.gnome) gnome-keyring;
+  inherit (config.modules.services) sops;
 in
 {
   services.openssh = {
-    generateHostKeys = !impermanence.enable;
+    generateHostKeys = !sops.enable;
 
     knownHosts."github.com".publicKey =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
