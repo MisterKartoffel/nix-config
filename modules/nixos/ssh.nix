0c6dcefa58ec4b2b2b9a04cdaa7377737c55815c
Felipe Duarte <felipesdrs@hotmail.com>
2026-07-02 14:52:35 -0300

    nixos/system: dissociated ssh host key path from etc overlay configuration

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -1,12 +1,17 @@
-{ config, ... }:
+{ config, lib, ... }:
 let
-  inherit (config.modules.services) impermanence;
-  inherit (config.services.gnome) gnome-keyring;
   inherit (config.modules.services) sops;
+  inherit (config.sops) secrets;
 in
 {
   services.openssh = {
     generateHostKeys = !sops.enable;
+    hostKeys = lib.optionals sops.enable [
+      {
+        path = secrets."ssh_key".path;
+        type = "ed25519";
+      }
+    ];
 
     knownHosts."github.com".publicKey =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
@@ -17,6 +22,4 @@ in
       KbdInteractiveAuthentication = false;
     };
   };
-
-  programs.ssh.startAgent = !gnome-keyring.enable;
 }
--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -1,8 +1,6 @@
 { config, lib, ... }:
 let
-  inherit (config.modules.services) sops;
   inherit (config.system.etc) overlay;
-  inherit (config.sops) secrets;
 in
 {
   system.etc.overlay = {
@@ -19,17 +17,4 @@ in
   # system.etc.overlay doesn't create /etc/NIXOS file
   # https://github.com/NixOS/nixpkgs/issues/341453
   environment.etc."NIXOS".text = lib.mkIf (overlay.enable && !overlay.mutable) "";
-
-  # sops-nix fails copying SSH host key to /etc if it's immutable
-  # fixed in modules/nixos/sops.nix
-
-  # sshd daemon fails to start if key is not found
-  # see issue above
-  services.openssh.hostKeys = lib.optionals sops.enable [
-    {
-      path = secrets."ssh_key".path;
-      type = "ed25519";
-    }
-  ];
-
 }
