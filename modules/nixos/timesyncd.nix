13401da1b3dcdf92b4034001dfe15ae948cc629e
Felipe Duarte <felipesdrs@hotmail.com>
2026-07-10 14:48:26 -0300

    nixos/timesyncd: circumvented offline -> online DNS failures

---
+++ modules/nixos/timesyncd.nix
@@ -0,0 +1,16 @@
+{ config, ... }: {
+  services.timesyncd = {
+    servers = config.networking.timeServers;
+
+    # If the system boots up without internet access,
+    # it fails to sync on boot and subsequent DNS queries
+    # fail due to invalid system time. Having these
+    # hardcoded Anycast IP addresses as fallback should
+    # mitigate this issue.
+    fallbackServers = [
+      "9.9.9.9" # Quad9
+      "162.159.200.1" # Cloudflare
+      "216.239.35.0" # Google
+    ];
+  };
+}
