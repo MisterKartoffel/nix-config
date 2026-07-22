54b8c1dd197792cfb52c9652aec952c2c78adc4f
Felipe Duarte <felipesdrs@hotmail.com>
2026-07-22 00:11:13 -0300

    migration: ssh

---
+++ modules/hjem/ssh.nix
@@ -0,0 +1,19 @@
+{ osConfig, ... }:
+let
+  inherit (osConfig.services) openssh;
+in
+{
+  files.".ssh/config" = {
+    inherit (openssh) enable;
+    text = ''
+      Host *
+      	AddKeysToAgent yes
+      	IdentitiesOnly yes
+      	IdentityFile ~/.ssh/id_ed25519
+
+      Host kindle
+      	HostName 192.168.0.202
+      	SetEnv TERM=linux
+      	'';
+  };
+}
