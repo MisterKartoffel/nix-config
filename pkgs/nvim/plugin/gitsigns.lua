2c37538d2084f984a48b2ae813fa5f048dae3545
Felipe Duarte <felipesdrs@hotmail.com>
2026-07-30 17:58:29 -0300

    pkgs/nvim: silent is already implied in utils.map

--- pkgs/nvim/plugin/gitsigns.lua
+++ pkgs/nvim/plugin/gitsigns.lua
@@ -14,7 +14,7 @@ gitsigns.setup({
       else
         gitsigns.nav_hunk("prev")
       end
-    end, { desc = "Jump to previous hunk", silent = true })
+    end, { desc = "Jump to previous hunk" })
 
     map("n", "]g", function()
       if vim.wo.diff then
@@ -22,19 +22,19 @@ gitsigns.setup({
       else
         gitsigns.nav_hunk("next")
       end
-    end, { desc = "Jump to next hunk", silent = true })
+    end, { desc = "Jump to next hunk" })
 
     -- Actions
-    map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Stage current hunk", silent = true })
-    map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Reset current hunk", silent = true })
+    map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Stage current hunk" })
+    map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Reset current hunk" })
     map("v", "<leader>gs", function()
       gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-    end, { desc = "Stage currently selected hunk", silent = true })
+    end, { desc = "Stage currently selected hunk" })
     map("v", "<leader>gr", function()
       gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-    end, { desc = "Reset currently selected hunk", silent = true })
-    map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "Stage current buffer", silent = true })
-    map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Reset current buffer", silent = true })
-    map("n", "<leader>gv", gitsigns.preview_hunk_inline, { desc = "Preview current hunk", silent = true })
+    end, { desc = "Reset currently selected hunk" })
+    map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "Stage current buffer" })
+    map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "Reset current buffer" })
+    map("n", "<leader>gv", gitsigns.preview_hunk_inline, { desc = "Preview current hunk" })
   end,
 })
