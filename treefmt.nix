7e5a95d939eb72d4549abf280b414690429fa30b
Felipe Duarte <felipesdrs@hotmail.com>
2026-08-01 15:31:48 -0300

    treefmt.nix: init deadnix, statix

--- treefmt.nix
+++ treefmt.nix
@@ -1,14 +1,33 @@
 { pkgs, lib, ... }: {
   settings = {
-    global.tree-root-file = "flake.nix";
+    global = {
+      tree-root-file = "flake.nix";
+      verbose = 2;
+    };
 
     formatter = {
-      nix = {
-        command = lib.getExe pkgs.nixfmt;
+      deadnix = {
+        command = lib.getExe pkgs.deadnix;
+        options = [ "--edit" ];
         includes = [ "*.nix" ];
+        priority = 1;
       };
 
-      lua = {
+      statix = {
+        command = lib.getExe pkgs.statix;
+        options = [ "fix" ];
+        includes = [ "*.nix" ];
+        priority = 2;
+        no-positional-arg-support = true;
+      };
+
+      nixfmt = {
+        command = lib.getExe pkgs.nixfmt;
+        includes = [ "*.nix" ];
+        priority = 3;
+      };
+
+      stylua = {
         command = lib.getExe pkgs.stylua;
         includes = [ "*.lua" ];
       };
