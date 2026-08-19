879683d2876b6418a34b58a7f059f7c2ce6db730
Felipe Duarte <felipesdrs@hotmail.com>
2026-08-19 19:16:29 -0300

    hjem/himalaya: init module

---
+++ modules/hjem/himalaya.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }: {
+  xdg.config.files."himalaya/config.toml" = {
+    enable = false;
+    generator = (pkgs.formats.toml { }).generate "himalaya-config.toml";
+
+    value = {
+      accounts.hotmail =
+        let
+          sasl.xoauth2 = {
+            username = "felipesdrs@hotmail.com";
+            token.command = [
+              "ortie"
+              "token"
+              "show"
+              "--account"
+              "hotmail"
+            ];
+          };
+        in
+        {
+          imap = {
+            server = "imaps://outlook.office365.com/";
+            inherit sasl;
+          };
+          smtp = {
+            server = "smtp://smtp.office365.com/";
+            starttls = true;
+            inherit sasl;
+          };
+        };
+    };
+  };
+}
