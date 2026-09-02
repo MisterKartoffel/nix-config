bdcf570ec77cebc67e888acab8ba051288e5f933
Felipe Duarte <felipesdrs@hotmail.com>
2026-09-02 18:00:05 -0300

    workflows: tests v2

--- ci/matrix.nix
+++ ci/matrix.nix
@@ -1,12 +1,26 @@
 let
+  /*
+    Imports default.nix, which is a flake-compat wrapper for flake.nix.
+
+    This avoids having to pass an absolute path to builtins.getFlake,
+    at the cost of having to fetch edolstra/flake-compat during the job.
+  */
   flake = import ../.;
 
+  /*
+    Since this matrix generator is host-centric, it maps each existing
+    pkgs.stdenv.hostPlatform.system to its GitHub Actions runner
+  */
   runners = {
     x86_64-linux = "ubuntu-latest";
     aarch64-linux = "ubuntu-24.04-arm";
     aarch64-darwin = "macos-latest";
   };
 
+  /*
+    Creates an attribute set of hosts by system architecture, dinamically
+    fetched from the nixosConfigurations and darwinConfigurations flake outputs.
+  */
   platforms = [
     "nixos"
     "darwin"
@@ -14,43 +28,54 @@ let
 
   hosts = builtins.concatMap (
     platform:
-    map (name: {
-      inherit name;
-      inherit (flake."${platform}Configurations".${name}.pkgs.stdenv.hostPlatform) system;
+    map (hostname: {
+      inherit hostname platform;
+      inherit (flake."${platform}Configurations".${hostname}.pkgs.stdenv.hostPlatform) system;
     }) (builtins.attrNames (flake."${platform}Configurations" or { }))
   ) platforms;
 
-  systems = builtins.mapAttrs (system: hosts: {
+  systems = builtins.groupBy (host: host.system) hosts;
+
+  # Helper for merging information common to all jobs into each output.
+  matrix = system: {
     inherit system;
-    inherit hosts;
-    devShells = builtins.attrNames (flake.devShells.${system} or { });
-    packages = builtins.attrNames (flake.packages.${system} or { });
     runner = runners.${system} or (abort "No runner defined for ${system}");
-  }) (builtins.groupBy (host: host.system) hosts);
+  };
 in
+/*
+  All these outputs are composed by the common attributes:
+  - system: architecture + kernel string, used for outputs keyed by system.
+  - runner: GitHub Actions runner, used in 'runs-on' for each of the matrix's jobs.
+
+  devShells, packages and hosts have unique "shell", "package", and "hostname"
+  attributes, respectively, for identifying the flake output when built.
+
+  Hosts have the unique attribute "platform", for composing the build command
+  in the runner's build job.
+*/
 {
   shells = builtins.concatMap (
     system:
-    map (shell: {
-      inherit shell system;
-      runner = systems.${system}.runner;
-    }) systems.${system}.devShells
-  ) (builtins.attrNames systems);
-
-  hosts = builtins.concatMap (
-    system:
-    map (host: {
-      hostname = host.name;
-      inherit (host) system;
-      runner = systems.${system}.runner;
-    }) systems.${system}.hosts
+    map (shell: (matrix system) // { inherit shell; }) (
+      builtins.attrNames (flake.devShells.${system} or { })
+    )
   ) (builtins.attrNames systems);
 
   packages = builtins.concatMap (
     system:
-    map (package: {
-      inherit package system;
-      runner = systems.${system}.runner;
-    }) systems.${system}.packages
+    map (package: (matrix system) // { inherit package; }) (
+      builtins.attrNames (flake.packages.${system} or { })
+    )
+  ) (builtins.attrNames systems);
+
+  hosts = builtins.concatMap (
+    system:
+    map (
+      host:
+      (matrix system)
+      // {
+        inherit (host) hostname platform;
+      }
+    ) systems.${system}
   ) (builtins.attrNames systems);
 }
