3b094f8f34c7a6429ef9102d4d582bf04221cbbc
Felipe Duarte <felipesdrs@hotmail.com>
2026-09-19 01:05:49 -0300

    ci/diff: improved logic, reduced output size

--- ci/diff.nix
+++ ci/diff.nix
@@ -1,85 +1,51 @@
 let
   data = builtins.fromJSON (builtins.readFile ./diff.json);
 
-  config = {
-    svg = {
-      colors = {
-        foreground = "#cdd6f4";
-        background = "#1e1e2e";
-        red = "#f38ba8";
-        green = "#a6e3a1";
-        blue = "#89b4fa";
-        orange = "#fab387";
-        yellow = "#f9e2af";
-        gray = "#6c7086";
-      };
-
-      font = {
-        family = "monospace";
-        size = 16;
-        height = 25;
-        width = 10;
-      };
-
-      layout = {
-        detailsX =
-          config.svg.layout.padding
-          +
-            (
-              helpers.max (
-                builtins.concatLists (
-                  map (
-                    section:
-                    map (
-                      diff:
-                      let
-                        status = config.status.${diff.status};
-                      in
-                      "[${status.marker}] ${diff.name}"
-                    ) section.diffs
-                  ) sections
-                )
-              )
-              + 1
-            )
-            * config.svg.font.width;
-
-        width =
-          config.svg.layout.detailsX
-          +
-            helpers.max (
-              builtins.concatLists (map (section: map (diff: (makeLine diff).plain) section.diffs) sections)
-            )
-            * config.svg.font.width;
-
-        padding = 16;
-      };
+  config = rec {
+    colors = {
+      foreground = "#cdd6f4";
+      background = "#1e1e2e";
+      red = "#f38ba8";
+      green = "#a6e3a1";
+      blue = "#89b4fa";
+      orange = "#fab387";
+      yellow = "#f9e2af";
+      gray = "#6c7086";
     };
+
+    font = {
+      family = "monospace";
+      size = 16;
+      width = 10;
+      height = 25;
+    };
+
+    padding = font.size;
 
     status = {
       Added = {
-        color = config.svg.colors.green;
-        marker = "A.";
+        color = colors.green;
+        marker = "[A.]";
       };
 
       Removed = {
-        color = config.svg.colors.red;
-        marker = "R.";
+        color = colors.red;
+        marker = "[R.]";
       };
 
       Upgraded = {
-        color = config.svg.colors.green;
-        marker = "U.";
+        color = colors.green;
+        marker = "[U.]";
       };
 
       Downgraded = {
-        color = config.svg.colors.red;
-        marker = "D.";
+        color = colors.red;
+        marker = "[D.]";
       };
 
       Changed = {
-        color = config.svg.colors.yellow;
-        marker = "C.";
+        color = colors.yellow;
+        marker = "[C.]";
       };
     };
 
@@ -103,7 +69,7 @@ let
     ];
   };
 
-  helpers = {
+  helpers = rec {
     escape =
       value:
       builtins.replaceStrings [ "&" "<" ">" "\"" "'" ] [ "&amp;" "&lt;" "&gt;" "&quot;" "&apos;" ] (
@@ -114,9 +80,9 @@ let
       {
         x,
         y,
-        color ? config.svg.colors.foreground,
-        font-family ? config.svg.font.family,
-        font-size ? config.svg.font.size,
+        color ? config.colors.foreground,
+        font-family ? config.font.family,
+        font-size ? config.font.size,
         content,
       }:
       ''
@@ -133,11 +99,8 @@ let
       {
         content,
         color ? null,
-        font-weight ? null,
       }:
-      "<tspan ${if color != null then ''fill="${color}"'' else ""} ${
-        if font-weight != null then ''font-weight="${font-weight}"'' else ""
-      }>${helpers.escape content}</tspan>";
+      "<tspan ${if color != null then ''fill="${color}"'' else ""}>${escape content}</tspan>";
 
     max =
       lines:
@@ -150,7 +113,7 @@ let
       ) 0 lines;
   };
 
-  format = {
+  format = rec {
     bytes =
       signed: bytes:
       let
@@ -175,27 +138,27 @@ let
       "${sign}${toString integer}.${decimal}${unit.suffix}";
 
     version =
-      version:
+      v:
       let
-        amount = version.amount or 1;
+        amount = v.amount or 1;
       in
-      version.name + (if amount == 1 then "" else " ×${toString amount}");
+      v.name + (if amount == 1 then "" else " ×${toString amount}");
 
     versions = builtins.map (
       v:
       {
         changed = {
-          old = format.version v.old;
-          new = format.version v.new;
+          old = version v.old;
+          new = version v.new;
         };
 
         added = {
           old = null;
-          new = format.version v.version;
+          new = version v.version;
         };
 
         removed = {
-          old = format.version v.version;
+          old = version v.version;
           new = null;
         };
 
@@ -209,146 +172,179 @@ let
   };
 
   diffLine =
-    {
-      name,
-      color,
-      marker,
-      oldVersions ? [ ],
-      newVersions ? [ ],
-      omitted ? false,
-      delta,
-    }:
+    diff:
     let
-      oldText = map (
-        value:
-        helpers.tspan {
-          content = value;
-          color = config.svg.colors.red;
-        }
-      ) oldVersions;
-
-      newText = map (
-        value:
-        helpers.tspan {
-          content = value;
-          color = config.svg.colors.green;
-        }
-      ) newVersions;
-
       render =
         text:
         builtins.concatStringsSep (helpers.tspan { content = ", "; }) (
-          if omitted then
+          if diff.has_omitted_versions then
             text
             ++ [
               (helpers.tspan {
                 content = "unchanged";
-                color = config.svg.colors.gray;
+                color = config.colors.gray;
               })
             ]
           else
             text
         );
 
-      versions =
-        if oldText == [ ] then
-          render newText
-        else if newText == [ ] then
-          render oldText
-        else
-          (render oldText) + helpers.tspan { content = " → "; } + (render newText);
+      old = {
+        list = builtins.filter (x: x != null) (map (v: v.old) versions.raw);
+        text = map (
+          content:
+          helpers.tspan {
+            inherit content;
+            color = config.colors.red;
+          }
+        ) old.list;
+        rendered = render old.text;
+      };
+
+      new = {
+        list = builtins.filter (x: x != null) (map (v: v.new) versions.raw);
+        text = map (
+          content:
+          helpers.tspan {
+            inherit content;
+            color = config.colors.green;
+          }
+        ) new.list;
+        rendered = render new.text;
+      };
+
+      versions = {
+        raw = format.versions diff.versions;
+        rendered =
+          if old.text == [ ] then
+            new.rendered
+          else if new.text == [ ] then
+            old.rendered
+          else
+            old.rendered + helpers.tspan { content = " → "; } + new.rendered;
+      };
 
       size =
-        if delta == 0 then
+        if diff.size_delta == 0 then
           ""
         else
           let
-            separator = if versions != "" then helpers.tspan { content = ", "; } else "";
-
-            color = if delta < 0 then config.svg.colors.blue else config.svg.colors.orange;
+            separator = if versions.rendered == "" then "" else helpers.tspan { content = ", "; };
+            color = if diff.size_delta < 0 then config.colors.blue else config.colors.orange;
           in
           separator
           + helpers.tspan {
-            content = format.bytes true delta;
+            content = format.bytes true diff.size_delta;
             inherit color;
           };
 
-      details = versions + size;
+      inherit (config.status.${diff.status}) marker color;
     in
     {
-      plain = toString (
-        builtins.filter (x: x != "") [
-          (
-            let
-              unchanged = if omitted then "unchanged" else "";
-              oldPlain = builtins.concatStringsSep ", " (oldVersions ++ [ unchanged ]);
-              newPlain = builtins.concatStringsSep ", " (newVersions ++ [ unchanged ]);
-            in
-            if oldVersions == [ ] then
-              newPlain
-            else if newVersions == [ ] then
-              oldPlain
-            else
-              "${oldPlain} → ${newPlain}"
-          )
-          (if delta != 0 then format.bytes true delta else "")
-        ]
-      );
+      plain = {
+        prefix = "${marker} ${diff.name}";
+
+        details = toString (
+          builtins.filter (x: x != "") [
+            (
+              let
+                unchanged = if diff.has_omitted_versions then [ "unchanged" ] else [ ];
+                oldPlain = builtins.concatStringsSep ", " (old.list ++ unchanged);
+                newPlain = builtins.concatStringsSep ", " (new.list ++ unchanged);
+              in
+              if old.list == [ ] then
+                newPlain
+              else if new.list == [ ] then
+                oldPlain
+              else
+                "${oldPlain} → ${newPlain}"
+            )
+            (if diff.size_delta != 0 then format.bytes true diff.size_delta else "")
+          ]
+        );
+      };
 
       svg = {
         marker = helpers.tspan {
-          content = "[${marker}]";
+          content = marker;
           inherit color;
         };
-        name = helpers.tspan { content = "${name}"; };
-        inherit details;
+        name = helpers.tspan { content = diff.name; };
+        details = versions.rendered + size;
       };
     };
 
-  makeLine =
-    diff:
-    let
-      versions = format.versions diff.versions;
-
-      oldVersions = builtins.filter (x: x != null) (map (v: v.old) versions);
-
-      newVersions = builtins.filter (x: x != null) (map (v: v.new) versions);
-    in
-    diffLine (
+  sections = {
+    raw = [
       {
-        inherit (diff) name;
-        inherit oldVersions newVersions;
-        omitted = diff.has_omitted_versions;
-        delta = diff.size_delta;
+        name = "CHANGED";
+        diffs = builtins.filter (
+          diff:
+          builtins.elem diff.status [
+            "Upgraded"
+            "Downgraded"
+            "Changed"
+          ]
+        ) data.diffs;
       }
-      // config.status.${diff.status}
-    );
+      {
+        name = "ADDED";
+        diffs = builtins.filter (diff: diff.status == "Added") data.diffs;
+      }
+      {
+        name = "REMOVED";
+        diffs = builtins.filter (diff: diff.status == "Removed") data.diffs;
+      }
+    ];
 
-  added = builtins.filter (diff: diff.status == "Added") data.diffs;
-  removed = builtins.filter (diff: diff.status == "Removed") data.diffs;
-  changed = builtins.filter (
-    diff:
-    builtins.elem diff.status [
-      "Upgraded"
-      "Downgraded"
-      "Changed"
-    ]
-  ) data.diffs;
+    processed = map (section: {
+      inherit (section) name;
+      lines = map diffLine section.diffs;
+    }) sections.raw;
 
-  sections = [
-    {
-      name = "CHANGED";
-      diffs = changed;
-    }
-    {
-      name = "ADDED";
-      diffs = added;
-    }
-    {
-      name = "REMOVED";
-      diffs = removed;
-    }
-  ];
+    rendered =
+      builtins.foldl'
+        (
+          acc: section:
+          let
+            res = renderSection acc.y section;
+          in
+          {
+            inherit (res) y;
+            svg = if res.svg == "" then acc.svg else acc.svg + "\n" + res.svg;
+          }
+        )
+        {
+          y = 32;
+          svg = "";
+        }
+        sections.processed;
+  };
+
+  sizes = rec {
+    prefix =
+      config.padding
+      +
+        (
+          helpers.max (
+            builtins.concatLists (map (section: map (line: line.plain.prefix) section.lines) sections.processed)
+          )
+          + 1
+        )
+        * config.font.width;
+
+    details =
+      (helpers.max (
+        builtins.concatLists (
+          map (section: map (line: line.plain.details) section.lines) sections.processed
+        )
+      ))
+      * config.font.width;
+
+    width = prefix + details;
+
+    height = sections.rendered.y + (builtins.length footer) * config.font.height;
+  };
 
   footer = [
     "PATHS: ${toString data.paths.old} → ${toString data.paths.new} (+${toString data.paths.added}, –${toString data.paths.removed})"
@@ -356,119 +352,84 @@ let
     "DIFF: ${format.bytes true (data.size_new - data.size_old)}"
   ];
 
-  renderDiff =
-    diff:
-    let
-      line = makeLine diff;
-    in
-    {
-      inherit (line.svg) marker name details;
-    };
-
   renderSection =
     y:
     {
       name,
-      diffs,
+      lines,
     }:
-    if diffs == [ ] then
+    if lines == [ ] then
       {
         inherit y;
         svg = "";
       }
     else
       let
-        rows = map renderDiff diffs;
-
-        nameRows = builtins.genList (
-          i:
-          let
-            row = builtins.elemAt rows i;
-          in
-          ''
+        nameRowsSvg = toString (
+          map (line: ''
             <tspan
-              x="${toString config.svg.layout.padding}"
-              dy="${toString config.svg.font.height}"
-            >${row.marker} ${row.name}</tspan>
-          ''
-        ) (builtins.length rows);
+              x="${toString config.padding}"
+              dy="${toString config.font.height}"
+            >${line.svg.marker} ${line.svg.name}</tspan>
+          '') lines
+        );
 
-        detailRows = builtins.genList (
-          i:
-          let
-            row = builtins.elemAt rows i;
-          in
-          ''
+        detailRowsSvg = toString (
+          map (line: ''
             <tspan
-              x="${toString config.svg.layout.detailsX}"
-              dy="${toString (if i == 0 then 0 else config.svg.font.height)}"
-            >${row.details}</tspan>
-          ''
-        ) (builtins.length rows);
-
+              x="${toString sizes.prefix}"
+              dy="${toString config.font.height}"
+            >${line.svg.details}</tspan>
+          '') lines
+        );
       in
       {
-        y = y + (builtins.length diffs + 2) * config.svg.font.height;
+        y = y + (builtins.length lines + 2) * config.font.height;
 
         svg =
           helpers.text {
-            x = toString config.svg.layout.padding;
-            y = toString y;
+            x = config.padding;
+            inherit y;
             content = ''
               <tspan
                 font-weight="bold">
                 ${helpers.escape name}
               </tspan>
-              ${toString nameRows}
+              ${nameRowsSvg}
             '';
           }
           + helpers.text {
-            x = toString config.svg.layout.padding;
-            y = toString (y + config.svg.font.height);
-            content = toString detailRows;
+            x = config.padding;
+            inherit y;
+            content = detailRowsSvg;
           };
       };
 
-  rendered =
-    let
-      changed = renderSection 32 (builtins.elemAt sections 0);
-      added = renderSection changed.y (builtins.elemAt sections 1);
-      removed = renderSection added.y (builtins.elemAt sections 2);
-      footerSvg = toString (
-        builtins.genList (
-          i:
-          helpers.text {
-            x = config.svg.layout.padding;
-            y = removed.y + i * config.svg.font.height;
-            content = builtins.elemAt footer i;
-          }
-        ) (builtins.length footer)
-      );
-    in
-    {
-      svg = builtins.concatStringsSep "\n" [
-        changed.svg
-        added.svg
-        removed.svg
-        footerSvg
-      ];
-
-      height = removed.y + builtins.length footer * config.svg.font.height + config.svg.layout.padding;
-    };
+  footerSvg = builtins.concatStringsSep "\n" (
+    builtins.genList (
+      i:
+      helpers.text {
+        x = config.padding;
+        y = sections.rendered.y + i * config.font.height;
+        content = builtins.elemAt footer i;
+      }
+    ) (builtins.length footer)
+  );
 in
 ''
   <?xml version="1.0" encoding="UTF-8"?>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="${toString config.svg.layout.width}"
-    height="${toString rendered.height}"
-    viewBox="0 0 ${toString config.svg.layout.width} ${toString rendered.height}"
+    width="${toString sizes.width}"
+    height="${toString sizes.height}"
+    viewBox="0 0 ${toString sizes.width} ${toString sizes.height}"
   >
     <rect
-      width="${toString config.svg.layout.width}"
-      height="${toString rendered.height}"
-      fill="${config.svg.colors.background}"
+      width="${toString sizes.width}"
+      height="${toString sizes.height}"
+      fill="${config.colors.background}"
     />
-    ${rendered.svg}
+    ${sections.rendered.svg}
+    ${footerSvg}
   </svg>
 ''
