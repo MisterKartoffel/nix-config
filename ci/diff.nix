3f56fc905c58f10a2a3214382d82b95eaacc4d0c
Felipe Duarte <felipesdrs@hotmail.com>
2026-09-26 00:17:14 -0300

    ci/diff.nix: further simplifications

--- ci/diff.nix
+++ ci/diff.nix
@@ -88,16 +88,19 @@ let
         x ? null,
         dy ? null,
         class ? null,
+        font-weight ? null,
         content,
       }:
       let
-        attrs = builtins.concatStringsSep "" [
+        attrs =
           (if x != null then " x=\"${toString x}\"" else "")
-          (if dy != null then " dy=\"${toString dy}\"" else "")
-          (if class != null then " class=\"${class}\"" else "")
-        ];
+          + (if dy != null then " dy=\"${toString dy}\"" else "")
+          + (if class != null then " class=\"${class}\"" else "")
+          + (if font-weight != null then " font-weight=\"${font-weight}\"" else "");
+
+        escaped = escape content;
       in
-      if attrs == "" then escape content else "<tspan${attrs}>${escape content}</tspan>";
+      if attrs == "" then escaped else "<tspan${attrs}>${escaped}</tspan>";
 
     max = builtins.foldl' (
       max: line:
@@ -108,7 +111,7 @@ let
     ) 0;
   };
 
-  format = rec {
+  format = {
     bytes =
       signed: bytes:
       let
@@ -120,115 +123,111 @@ let
           else if bytes > 0 then
             "+"
           else
-            "–";
+            "-";
 
         unit = builtins.head (builtins.filter (u: abs + 1 >= u.size) config.units);
 
-        scaled = (abs * 100 + unit.size / 2) / unit.size;
+        scaled = (abs * 100) / unit.size;
         integer = scaled / 100;
-        fraction = scaled - integer * 100;
-
-        decimal = if fraction < 10 then "0${toString fraction}" else toString fraction;
+        decimal =
+          let
+            fraction = scaled - integer * 100;
+          in
+          if fraction < 10 then "0${toString fraction}" else toString fraction;
       in
-      "${sign}${toString integer}.${decimal}${unit.suffix}";
+      "${sign}${toString integer}.${decimal} ${unit.suffix}";
 
-    version =
-      v:
+    versions =
       let
-        amount = v.amount or 1;
+        string =
+          {
+            name,
+            amount ? 1,
+          }:
+          name + (if amount == 1 then "" else " ×${toString amount}");
+
+        kinds = {
+          changed = v: {
+            old = string v.old;
+            new = string v.new;
+          };
+
+          added = v: {
+            old = null;
+            new = string v.version;
+          };
+
+          removed = v: {
+            old = string v.version;
+            new = null;
+          };
+
+          amount_changed = v: {
+            old = "${v.version.name} ×${toString v.old_amount}";
+            new = "${v.version.name} ×${toString v.new_amount}";
+          };
+        };
       in
-      v.name + (if amount == 1 then "" else " ×${toString amount}");
-
-    versions = builtins.map (
-      v:
-      {
-        changed = {
-          old = version v.old;
-          new = version v.new;
-        };
-
-        added = {
-          old = null;
-          new = version v.version;
-        };
-
-        removed = {
-          old = version v.version;
-          new = null;
-        };
-
-        amount_changed = {
-          old = "${v.version.name} ×${toString v.old_amount}";
-          new = "${v.version.name} ×${toString v.new_amount}";
-        };
-      }
-      .${v.kind} or (throw "Unknown version kind: ${v.kind}")
-    );
+      builtins.map (v: kinds.${v.kind} v);
   };
 
   render = {
     versions =
-      items: x:
+      items: omitted: class: x:
       if items == [ ] then
         ""
       else
         let
-          first = builtins.head items;
-          rest = builtins.tail items;
+          first = helpers.tspan {
+            inherit x class;
+            content = builtins.head items;
+          };
 
-          tspan =
-            first: item:
+          rest = map (
+            content:
             helpers.tspan {
-              x = if first then x else null;
-              inherit (item) class content;
-            };
+              inherit class content;
+            }
+          ) (builtins.tail items);
+
+          unchanged =
+            if !omitted then
+              [ ]
+            else
+              [
+                (helpers.tspan {
+                  content = "unchanged";
+                  class = "gr";
+                })
+              ];
         in
-        builtins.concatStringsSep ", " ([ (tspan true first) ] ++ (map (tspan false) rest));
+        builtins.concatStringsSep ", " ([ first ] ++ rest ++ unchanged);
 
     line =
       diff:
       let
-        transform =
-          class: list:
-          let
-            items = map (content: { inherit content class; }) list;
-          in
-          if diff.has_omitted_versions then
-            items
-            ++ [
-              {
-                content = "unchanged";
-                class = "gr";
-              }
-            ]
-          else
-            items;
+        generate = field: class: rec {
+          list = builtins.filter (x: x != null) (builtins.catAttrs field versions.raw);
+          rendered = dx: render.versions list omitted class dx;
+        };
 
         versions.raw = format.versions diff.versions;
 
-        old = {
-          list = builtins.filter (x: x != null) (map (v: v.old) versions.raw);
-          items = transform "r" old.list;
-        };
+        old = generate "old" "r";
+        new = generate "new" "g";
 
-        new = {
-          list = builtins.filter (x: x != null) (map (v: v.new) versions.raw);
-          items = transform "g" new.list;
-        };
-
-        status = {
-          inherit (config.status.${diff.status}) marker class;
-        };
+        omitted = diff.has_omitted_versions;
+        status = config.status.${diff.status};
       in
       {
         text = {
           prefix = "${status.marker} ${diff.name}";
 
-          details = builtins.concatStringsSep "" (
-            builtins.filter (x: x != "") [
-              (
+          details =
+            let
+              versions =
                 let
-                  unchanged = if diff.has_omitted_versions then [ "unchanged" ] else [ ];
+                  unchanged = if omitted then [ "unchanged" ] else [ ];
                   oldPlain = builtins.concatStringsSep ", " (old.list ++ unchanged);
                   newPlain = builtins.concatStringsSep ", " (new.list ++ unchanged);
                 in
@@ -237,11 +236,11 @@ let
                 else if new.list == [ ] then
                   oldPlain
                 else
-                  "${oldPlain} → ${newPlain}"
-              )
-              (if diff.size_delta == 0 then "" else format.bytes true diff.size_delta)
-            ]
-          );
+                  "${oldPlain} → ${newPlain}";
+
+              size = if diff.size_delta == 0 then "" else format.bytes true diff.size_delta;
+            in
+            versions + size;
         };
 
         svg =
@@ -255,12 +254,12 @@ let
             };
 
             versions =
-              if old.items != [ ] && new.items != [ ] then
-                "${render.versions old.items dx} → ${render.versions new.items null}"
-              else if old.items != [ ] then
-                render.versions old.items dx
-              else if new.items != [ ] then
-                render.versions new.items dx
+              if old.list != [ ] && new.list != [ ] then
+                "${old.rendered dx} → ${new.rendered null}"
+              else if old.list != [ ] then
+                old.rendered dx
+              else if new.list != [ ] then
+                new.rendered dx
               else
                 "";
 
@@ -296,27 +295,25 @@ let
       else
         let
           title = ''<tspan x="${toString config.padding}" font-weight="bold">${helpers.escape category}</tspan>'';
-
-          body = builtins.concatStringsSep "" (map (line: "\n" + line.svg dimensions.prefix) lines);
+          body = map (line: line.svg dimensions.prefix) lines;
         in
         {
           y = y + (builtins.length lines + 2) * config.font.height;
 
           svg = helpers.text {
             inherit y;
-            content = title + body;
+            content = builtins.concatStringsSep "\n" ([ title ] ++ body);
           };
         };
   };
 
   diffs = builtins.groupBy (
     diff:
-    if diff.status == "Added" then
-      "ADDED"
-    else if diff.status == "Removed" then
-      "REMOVED"
-    else
-      "CHANGED"
+    {
+      Added = "ADDED";
+      Removed = "REMOVED";
+    }
+    .${diff.status} or "CHANGED"
   ) data.diffs;
 
   sections = {
@@ -382,16 +379,12 @@ let
     in
     rec {
       prefix = config.padding + (helpers.max (builtins.catAttrs "prefix" text) + 1) * config.font.width;
-
       details = (helpers.max (builtins.catAttrs "details" text) - 2) * config.font.width;
-
       width = prefix + details;
       height = sections.rendered.y + (builtins.length sections.footer.list + 1) * config.font.height;
     };
-
 in
 ''
-  <?xml version="1.0" encoding="UTF-8"?>
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="${toString dimensions.width}"
